a229be75215d42905f906cb7d4e59fb20bf7da2f
syugoudada <to154514asa134009@gmail.com>
2021-02-07 10:05:08 +0900

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+//webpackの設定
+module.exports = {
+  entry: {
+    bundle: './src/index.ts'
+  },
+  output:{
+    path: path.join(__dirname,'dist'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.ts','.js']
+  },
+  devServer: {
+    contentBase: path.join(__dirname,'dist'),
+    open: true
+  },
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.ts$/
+      }
+    ]
+  }
+}
